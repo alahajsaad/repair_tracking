442f8b18fa30c49951ebc452bd71edaced3cead5
ala <alahajsaad398@gmail.com>
2025-07-02 09:56:30 +0100

    fix: correct vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
+import { fileURLToPath } from 'url';
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
-      "@": path.resolve(__dirname, "src"), // Ensure the alias points to "src"
+      "@": path.resolve(__dirname, "src"),
     },
   },
 })
